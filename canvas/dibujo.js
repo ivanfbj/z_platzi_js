a91636d0526c152fe1bc9ac4d56df7e83e07897e
Ivan Fernando Bedoya Jaramillo <ivanfbj@gmail.com>
2020-08-17 17:20:19 -0500

    15_Ciclos while y for en JavaScript

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,14 +1,39 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d"); //getContext es una función del objeto Canvas que me permite obtener el área donde voy a dibujar, se dibuja en 2d.
+var lineas = 30;
+var l = 0;
+var yi, xf;
 
-lienzo.beginPath(); //beginPath es una función que me permite iniciar el trazo.
-lienzo.strokeStyle = "blue"; //strokeStyle es una propiedad para escoger el color
-lienzo.moveTo(0, 0); //moveTo es una función del canvas para mover el lapiz en donde va a arrancar la linea. Es el punto inicial.
-lienzo.lineTo(150, 150); //lineTo es la funcion de trazar la linea. Es el punto final.
-lienzo.stroke(); //stroke es la función que dibuja la linea del inicio al final
-lienzo.closePath(); //clasePath es la función que me permite cerrar el trazo.
+while (l < lineas) {
+  yi = 10 * l;
+  xf = 10 * (l + 1);
+  dibujarLinea("blue", 0, yi, xf, 300);
+  console.log(`linea ${l}`);
+  l++;
+}
 
-/*3 ejemplos diferentes para crear una función */
+for (l = 0; l < lineas; l++) {
+  yi = 10 * l;
+  xf = 10 * (l + 1);
+  dibujarLinea("blue", 300, yi, xf, 0);
+}
+
+for (l = 0; l < lineas; l++) {
+  xi = 10 * l;
+  yf = 10 * (l + 1);
+  dibujarLinea("blue", xi, 300, 300, 300 - yf);
+}
+
+for (l = 0; l < lineas; l++) {
+  xf = 10 * l;
+  yi = 10 * (l + 1);
+  dibujarLinea("blue", 0, yi, 300 - xf, 0);
+}
+
+dibujarLinea("white", 0, 0, 300, 300);
+dibujarLinea("white", 0, 300, 300, 0);
+dibujarLinea("white", 0, 150, 300, 150);
+dibujarLinea("white", 150, 0, 150, 300);
 
 function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal) {
   lienzo.beginPath();
@@ -19,31 +44,11 @@ function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal) {
   lienzo.closePath();
 }
 
-const dibujarLinea2 = (color, xInicial, yInicial, xFinal, yFinal) => {
-  lienzo.beginPath();
-  lienzo.strokeStyle = color;
-  lienzo.moveTo(xInicial, yInicial);
-  lienzo.lineTo(xFinal, yFinal);
-  lienzo.stroke();
-  lienzo.closePath();
-};
-
-const dibujarLinea3 = function (color, xInicial, yInicial, xFinal, yFinal) {
-  lienzo.beginPath();
-  lienzo.strokeStyle = color;
-  lienzo.moveTo(xInicial, yInicial);
-  lienzo.lineTo(xFinal, yFinal);
-  lienzo.stroke();
-  lienzo.closePath();
-};
-
-// dibujarLinea("red", 300, 0, 150, 150);
-// dibujarLinea2("yellow", 300, 300, 150, 150);
-// dibujarLinea3("green", 0, 300, 150, 150);
-
-for (var i = 0; i <= 300; i += 10) {
-  dibujarLinea2("blue", 0, i, 150, 150);
-  dibujarLinea2("green", i, 300, 150, 150);
-  dibujarLinea2("yellow", 300, i, 150, 150);
-  dibujarLinea2("red", i, 0, 150, 150);
-}
+// const dibujarLinea2 = (color, xInicial, yInicial, xFinal, yFinal) => {
+//   lienzo.beginPath();
+//   lienzo.strokeStyle = color;
+//   lienzo.moveTo(xInicial, yInicial);
+//   lienzo.lineTo(xFinal, yFinal);
+//   lienzo.stroke();
+//   lienzo.closePath();
+// };
