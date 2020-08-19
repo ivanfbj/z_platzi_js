292e26ea651a3b2e8e8930110f6a398a55b70cc3
Ivan Fernando Bedoya Jaramillo <ivanfbj@gmail.com>
2020-08-18 21:53:38 -0500

    17_Detectar_eventos_del_teclado_javascript

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,48 +1,47 @@
-var texto = document.getElementById("texto_lineas");
-var boton = document.getElementById("btn");
-var reset = document.getElementById("btn-reset");
-boton.addEventListener("click", dibujoPorClick);
-reset.addEventListener("click", resetPage);
-/*addEventListener es una función que está escuchando cuando suceda un evento
- y ejecutar alguna indicación. 
- Esta función necesita 2 parametros, 
- el primer parametro es a cual evento va a reaccionar en este caso el evento "click" 
- y el segundo parametro es lo que el va a hacer, en este caso ejecuta una función que hemos creado.
- */
-var d = document.getElementById("dibujito"); //Obteniendo la etiqueta <canvas> del HTML con base en su ID
-var lienzo = d.getContext("2d"); //getContext es una función del objeto Canvas que me permite obtener el área donde voy a dibujar, se dibuja en 2d.
-var ancho = d.width; //Se obtiene el ancho de la etiqueta <canvas> que se guardo en la variable "d".
+var reset = document.getElementById("btn-reload");
+//reset.addEventListener("click",reloadBtn)
+//function reloadBtn() {location.reload();}
+reset.addEventListener("click",function() {location.reload();})
+//document.getElementById("btn-reload").addEventListener("click",function() {location.reload();})
 
-function resetPage() {
-  location.reload();
+
+var teclas = {
+	LEFT: 37,
+	UP: 38,
+	RIGHT: 39,
+	DOWN: 40
+};
+
+document.addEventListener("keyup",dibujarTeclado);
+
+function dibujarTeclado(evento) {
+	if (evento.keyCode == teclas.LEFT){
+		console.log("hacía izquierda");
+	}
+
+	switch (evento.keyCode){
+		case teclas.UP: 
+		console.log("hacía arriba");
+		break;
+		case teclas.RIGHT:
+		console.log("hacía derecha");
+		break;
+		case teclas.DOWN:
+		console.log("hacía abajo");
+		break;
+		case teclas.LEFT:
+		break;
+		default:
+		console.log("Es otra tecla");
+		break;
+	}
 }
 
-function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal) {
-  lienzo.beginPath();
-  lienzo.strokeStyle = color;
-  lienzo.moveTo(xInicial, yInicial);
-  lienzo.lineTo(xFinal, yFinal);
-  lienzo.stroke();
-  lienzo.closePath();
-}
+/*
+evento.key + " es "+evento.keyCode
+ArrowLeft 37
+ArrowUp 38
+ArrowRight 39
+ArrowDown 40
 
-function dibujoPorClick() {
-  var lineas = parseInt(texto.value);
-  var l = 0;
-  var x, y;
-  var espacio = ancho / lineas;
-
-  for (l = 0; l < lineas; l++) {
-    y = espacio * l;
-    x = espacio * (l + 1);
-    dibujarLinea("blue", 0, y, x, 300);
-    dibujarLinea("blue", 300, y, x, 0);
-    dibujarLinea("blue", x, 300, 300, 300 - y);
-    dibujarLinea("blue", 0, y, 300 - x, 0);
-  }
-
-  dibujarLinea("white", 0, 0, 300, 300);
-  dibujarLinea("white", 0, 300, 300, 0);
-  dibujarLinea("white", 0, 150, 300, 150);
-  dibujarLinea("white", 150, 0, 150, 300);
-}
+*/
